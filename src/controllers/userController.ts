7b7146094cd69fc9ea010d3a5b27da627352acba
Pablo <pncar94@gmail.com>
2025-01-23 21:43:51 -0300

    Second Commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express';
 import db from '../db/database';
 import { faker } from '@faker-js/faker';
-import { getUsers2 } from "../models/userModel";
+import User from "../models/userModel";
 
     export const getUsers = async (req: Request, res: Response) => {
         try {
-        //const query = 'SELECT * FROM users';
-        //const [rows] = await db.query(query);
-        const rows = await getUsers2();
+        const rows = await User.getUsers();
         res.status(200).json(rows);
         } catch (error) {
         console.error(error);
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,8 +1,12 @@
 import db from '../db/database';
 import { faker } from '@faker-js/faker';
 
-export const getUsers2 = async () => {
-    const query = 'SELECT * FROM users';
-    const [rows] = await db.query(query);
-    return rows;
-};
+class User {
+    static async getUsers() {
+      const query = 'SELECT * FROM users';
+      const [rows] = await db.query(query);
+      return rows;
+    }
+}
+
+export default User;
