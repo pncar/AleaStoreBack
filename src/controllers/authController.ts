9f59224c9cba821aea52844ef674e9cc2dfcca8d
Pablo <pncar94@gmail.com>
2025-04-03 19:58:08 -0300

    JWT debugging

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -32,7 +32,8 @@ export const login = async (req: Request, res: Response): Promise<any> => {
     const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, SECRET, { expiresIn: "1h" });
     console.log("TOKEN: ");
     console.log(token);
-    res.cookie("authToken", token, { httpOnly: true, secure: true }).json({ message: "Logged in" });
+    //res.cookie("authToken", token, { httpOnly: true, secure: true }).json({ message: "Logged in" });
+    res.cookie("authToken", token, { httpOnly: true, secure: false, sameSite: "lax"}).json({message: `Logged in`});
 };
 
 export const logout = (req: Request, res: Response): void => {
